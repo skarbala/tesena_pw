7672223683d7846bb5166d79a0795e28ebd80c0d
martinskarbala <martin@supernova.io>
2025-06-26 13:47:37 +0200

    adds structure for api

--- tests/sortingHat.spec.ts
+++ tests/sortingHat.spec.ts
@@ -1,50 +1,57 @@
 import test, { expect } from '@playwright/test'
 
 test.describe('Sorting hat', () => {
+    test.describe('UI', () => {
 
-    test('user is sorted after clicking on sort me button', async ({ page }) => {
-        await page.goto('/#/sortingHat')
-        await page.locator('[data-test="sort-button"]').click()
+        test('user is sorted after clicking on sort me button', async ({ page }) => {
+            await page.goto('/#/sortingHat')
+            await page.locator('[data-test="sort-button"]').click()
 
-        const response = await page.waitForResponse('**/sortingHat', { timeout: 5000 })
-        const json = await response.json() as SortingHatResponse
+            const response = await page.waitForResponse('**/sortingHat', { timeout: 5000 })
+            const json = await response.json() as SortingHatResponse
 
-        await expect(page.locator('[data-test="result-message"]')).toBeVisible()
-        await expect(page.locator('[data-test="result-message"]')).toHaveText(json.sortingHatSays)
+            await expect(page.locator('[data-test="result-message"]')).toBeVisible()
+            await expect(page.locator('[data-test="result-message"]')).toHaveText(json.sortingHatSays)
 
-        await expect(page.locator('[data-test="house-result"]')).toBeVisible()
-        await expect(page.locator('[data-test="house-result"]')).toHaveText(json.house)
-    })
-
-    test('display fake message', async ({ page }) => {
-        const fakeMessage: SortingHatResponse = {
-            sortingHatSays: 'This is a fake message you fools!',
-            house: 'Mordor'
-        }
-        await page.route('**/sortingHat', async route => {
-            await route.fulfill({
-                body: JSON.stringify(fakeMessage)
-            })
+            await expect(page.locator('[data-test="house-result"]')).toBeVisible()
+            await expect(page.locator('[data-test="house-result"]')).toHaveText(json.house)
         })
-        await page.goto('/#/sortingHat')
-        await page.locator('[data-test="sort-button"]').click()
 
-        await expect(page.locator('[data-test="result-message"]')).toBeVisible()
-        await expect(page.locator('[data-test="result-message"]')).toHaveText(fakeMessage.sortingHatSays)
-
-        await expect(page.locator('[data-test="house-result"]')).toBeVisible()
-        await expect(page.locator('[data-test="house-result"]')).toHaveText(fakeMessage.house)
-    })
-
-    test('display message when server returns 500', async ({ page }) => {
-        await page.route('**/sortingHat', async route => {
-            await route.fulfill({
-                status: 500
+        test('display fake message', async ({ page }) => {
+            const fakeMessage: SortingHatResponse = {
+                sortingHatSays: 'This is a fake message you fools!',
+                house: 'Mordor'
+            }
+            await page.route('**/sortingHat', async route => {
+                await route.fulfill({
+                    body: JSON.stringify(fakeMessage)
+                })
             })
+            await page.goto('/#/sortingHat')
+            await page.locator('[data-test="sort-button"]').click()
+
+            await expect(page.locator('[data-test="result-message"]')).toBeVisible()
+            await expect(page.locator('[data-test="result-message"]')).toHaveText(fakeMessage.sortingHatSays)
+
+            await expect(page.locator('[data-test="house-result"]')).toBeVisible()
+            await expect(page.locator('[data-test="house-result"]')).toHaveText(fakeMessage.house)
         })
-        await page.goto('/#/sortingHat')
-        await page.locator('[data-test="sort-button"]').click()
+
+        test('display message when server returns 500', async ({ page }) => {
+            await page.route('**/sortingHat', async route => {
+                await route.fulfill({
+                    status: 500
+                })
+            })
+            await page.goto('/#/sortingHat')
+            await page.locator('[data-test="sort-button"]').click()
+        })
     })
+
+    test.describe('API', () => {
+
+    })
+
 })
 
 interface SortingHatResponse {
